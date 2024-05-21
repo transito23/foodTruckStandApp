6ddb7210bdb11faf8b55f9462625e287d45bc012
Ivan Transito <itransito@mail.csuchico.edu>
2024-05-21 14:26:47 -0700

    dependices and plugins for app

--- foodapptruck/macos/Flutter/GeneratedPluginRegistrant.swift
+++ foodapptruck/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,9 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import geolocator_apple
+import path_provider_foundation
+import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
